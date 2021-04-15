ce5f0341d9629a72c269789753ef1e0eba852172
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-04-14 21:51:40 -0400

    create post model and replace dummy data with post model data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-posts = [
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted': 'Aug 27, 2018'
-    
-    },
-    {
-        'author':'EAGee',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted': 'April 8, 2021'
-    
-    }
-]
 
 
 def home(request):
-    context = {'posts':posts}
+    context = {'posts':Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about(request):
