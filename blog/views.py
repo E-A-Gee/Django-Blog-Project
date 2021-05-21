6bfedb13a036ab4435b032451f14097967d03283
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-05-21 18:06:15 -0400

    replace home view with class based view and add detail view for posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
+class PostListView(ListView):
+    model=Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-def home(request):
-    context = {'posts':Post.objects.all()}
-    return render(request, 'blog/home.html', context)
+class PostDetailView(DetailView):
+    model=Post
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
