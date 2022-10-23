c0583d781d5e52f06e9fd6f2c446e4617434c648
Liz Gee <70771300+E-A-Gee@users.noreply.github.com>
2022-10-23 16:53:49 -0400

    Replace Heroku as Host with Railway

--- django_project_too/settings.py
+++ django_project_too/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
-ALLOWED_HOSTS = ['geelizherokuapp.herokuapp.com']
+ALLOWED_HOSTS = ['geeliz-superhero-blog.up.railway.app']
 
 
 # Application definition
